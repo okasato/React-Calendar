05a2acf53ced57475739d463ec3254d345a98a46
Shingo Konnai <k1ckap0l03@gmail.com>
2018-08-02 16:18:59 -0700

    add app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const app = express();
+
+app.use("*",(req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+const server = app.listen(PORT, () => {
+  console.log(`Server up and listening on port ${PORT}`);
+});
+
+module.exports = app;
